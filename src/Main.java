ac69276afae7c8b62f52155a31368ef868956991
Guna <Gunaseelan.MUTHAPPAN@transport.nsw.gov.au>
2017-05-18 18:22:30 +1000

    buildmxl

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     private int port = 8889;
     private String projectBaseDirectory = "/Users/mcc1/workspace/project3iphonenew";
     protected Client client = null;
-    //sample...test1
+    //sample...test12345
 
     @Before
     public void setUp(){
